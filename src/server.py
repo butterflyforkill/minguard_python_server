4233e432d75490d4f23c9792832877eebb5c2ce4
butterflyforkill <vasse.paul@gmail.com>
2025-04-09 16:52:49 +0200

    change the structure of the folders, change the server.py file

---
+++ src/server.py
@@ -0,0 +1,24 @@
+
+from fastapi import FastAPI
+import uvicorn
+
+version = "v1"
+description = "MindGuard Python Server"
+title = "MindGuard Python Server"
+version_prefix = "/api/{version}"
+
+
+app = FastAPI(
+    title=title,
+    description=description,
+    version=version,
+    docs_url=f"{version_prefix}/docs",
+    redoc_url=f"{version_prefix}/redoc",
+)
+
+@app.get("/hello")
+async def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
